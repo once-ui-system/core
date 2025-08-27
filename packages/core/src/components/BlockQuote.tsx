5628d5273a1eaec98da96d880d28dcefee0c55c3
Lorant <lorant@once-ui.com>
2025-08-27 13:52:09 +0200

    refact: BlockQuote style

--- packages/core/src/components/BlockQuote.tsx
+++ packages/core/src/components/BlockQuote.tsx
@@ -1,34 +1,83 @@
 import { forwardRef } from "react";
-import { Flex } from ".";
+import { Avatar, Column, Line, Row, SmartLink, Text } from ".";
 
-interface BlockQuoteProps extends React.ComponentProps<typeof Flex> {
+interface BlockQuoteProps extends React.ComponentProps<typeof Column> {
   children: React.ReactNode;
-  className?: string;
+  preline?: React.ReactNode;
+  subline?: React.ReactNode;
+  separator?: "top" | "bottom" | "both" | "none";
+  author?: {
+    name?: React.ReactNode,
+    avatar?: string,
+  };
+  link?: { href: string, label: string };
   style?: React.CSSProperties;
+  className?: string;
+  align?: "center" | "left" | "right";
 }
 
 const BlockQuote = forwardRef<HTMLDivElement, BlockQuoteProps>(
-  ({ children, className, style, ...rest }, ref) => {
+  ({ children, className, style, preline, subline, author, link, align = "center", separator = "both", ...flex }, ref) => {
     return (
-      <Flex
-        ref={ref}
-        as="blockquote"
-        radius="m"
-        margin="0"
-        border="brand-alpha-medium"
-        background="brand-alpha-weak"
-        borderStyle="dashed"
-        overflow="hidden"
-        paddingY="20"
-        paddingX="24"
-        onBackground="brand-medium"
-        fillWidth
-        style={style}
-        className={className}
-        {...rest}
-      >
-        {children}
-      </Flex>
+      <Column fillWidth horizontal="center" gap="24">
+        {(separator === "top" || separator === "both") && (
+          <Row fillWidth horizontal="center">
+            <Line width="40"/>
+          </Row>
+        )}
+        <Column
+          ref={ref}
+          as="blockquote"
+          fillWidth
+          marginY="32"
+          marginX="0"
+          horizontal={align === "left" ? "start" : align === "right" ? "end" : "center"}
+          align={align}
+          style={style}
+          className={className}
+          {...flex}
+        >
+          {preline && (
+            <Text onBackground="neutral-weak" marginBottom="32">
+              {preline}
+            </Text>
+          )}
+          <Text variant="heading-strong-xl" wrap="balance">
+            {children}
+          </Text>
+          {subline && (
+            <Text onBackground="neutral-weak" marginTop="24">
+              {subline}
+            </Text>
+          )}
+          {(author || link) && (
+            <Row gap="12" center marginTop="32">
+              —
+              {author?.avatar && (
+                <Avatar size="s" src={author?.avatar} />
+              )}
+              {author?.name && (
+                <Text variant="label-default-s">{author?.name}</Text>
+              )}
+              {link?.href && (
+                <Row as="cite">
+                  <SmartLink
+                    unstyled
+                    href={link?.href && (/^https?:\/\//.test(link.href) ? link.href : `https://${link.href}`)}
+                  >
+                    <Text variant="label-default-s">{link?.label || link?.href}</Text>
+                  </SmartLink>
+                </Row>
+              )}
+            </Row>
+          )}
+        </Column>
+        {(separator === "bottom" || separator === "both") && (
+          <Row fillWidth horizontal="center">
+            <Line width="40"/>
+          </Row>
+        )}
+      </Column>
     );
   },
 );
