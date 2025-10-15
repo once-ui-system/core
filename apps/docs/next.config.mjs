b2c43919225ce53a99ecc621a6f0fbcca6b5b712
Lorant <lorant@once-ui.com>
2025-10-15 20:20:31 +0200

    docs: add redirects

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -68,6 +68,41 @@ const nextConfig = {
         destination: '/:slug*',
         permanent: true,
       },
+      {
+        source: '/magic-portfolio/:slug*',
+        destination: '/products/magic-portfolio/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/magic-docs/:slug*',
+        destination: '/products/magic-docs/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/magic-bio/:slug*',
+        destination: '/products/magic-bio/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/magic-agent/:slug*',
+        destination: '/products/magic-agent/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/magic-convert/:slug*',
+        destination: '/products/magic-convert/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/magic-store/:slug*',
+        destination: '/products/magic-store/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/supabase-starter/:slug*',
+        destination: '/products/supabase-starter/:slug*',
+        permanent: true,
+      },
     ];
   },
 };
