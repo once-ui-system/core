8246ab42cc5643f96b1e2ea16a944035c617a193
Lorant <lorant@once-ui.com>
2026-03-12 17:46:30 +0100

    docs: add example for CountFx to use with the new useInViewport hook

--- apps/docs/src/product/CountFxExample.tsx
+++ apps/docs/src/product/CountFxExample.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { Row, CountFx } from "@once-ui-system/core";
+import { useState, useEffect, useRef } from 'react';
+import { Row, CountFx, useInViewport } from "@once-ui-system/core";
 
 export const CountFxExample = () => {
   const [value, setValue] = useState<number>(0);
@@ -20,3 +20,29 @@ export const CountFxExample = () => {
     </Row>
   );
 };
+
+
+export const ScrollCountExample = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [value, setValue] = useState<number>(0);
+
+  // Trigger when element reaches center of viewport
+  const isInViewport = useInViewport(ref, { rootMargin: "-50% 0px" });
+  
+  useEffect(() => {
+      if (isInViewport) {
+          setValue(9876);
+      }
+  }, [isInViewport]);
+  
+  return (
+    <CountFx
+      ref={ref}
+      variant="display-strong-l"
+      value={value}
+      speed={2000}
+      effect="wheel"
+      easing="ease-out"
+    />
+  );
+}
